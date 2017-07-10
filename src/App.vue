e953c0957e7af438b2cfecb87291952a17bd04e1
luosl <lsr1962@126.com>
2017-07-10 15:26:54 +0800

    首页

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,83 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <mt-header :style="{ paddingTop: iosFixed, height: iosFixedHeight, fontSize: '20px' }" :title="$route.meta.title" v-if="!($route.meta.home)">
+      <div slot="left" class="backBtn" @click="goBack"></div>
+      <div v-if="$route.meta.right" slot="right" @click="triggerRight">{{$route.meta.right}}</div>
+    </mt-header>
+    <transition name="merchant-slide" mode="out-in">
+      <div ref="trans">
+        <router-view></router-view>
+      </div>
+    </transition>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  import { Header } from 'mint-ui'
+  export default {
+    mounted () {
+      if (window.navigator.userAgent.indexOf('iPhone') > -1) {
+        this.iosFixed = '20px'
+        this.iosFixedHeight = '64px'
+      }
+      this.$refs.trans.style.height = (document.documentElement.clientHeight - this.$refs.trans.getBoundingClientRect().top) + 'px'
+      this.$refs.trans.style.overflow = 'auto'
+    },
+    components: {
+      [Header.name]: Header
+    },
+    data () {
+      return {
+        iosFixed: '0px',
+        iosFixedHeight: '44px'
+      }
+    },
+    name: 'app',
+    methods: {
+      goBack () {
+        this.$router.back()
+      },
+      triggerRight () {
+        this.$emit(this.$route.meta.right)
+      }
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import './style/reset.css';
+  #app {
+    margin: 0;
+    padding: 0;
+    color: #2c3e50;
+  }
+  .backBtn {
+    background: url(./assets/btn_back@2x.png) no-repeat;
+    background-size: 11px 18px;
+    background-position: center left;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 44px;
+  }
+  .mint-header {
+    height: 44px;
+    line-height: 44px;
+    background: -moz-linear-gradient(left, #3FC2F1  0%, #3479DE 100%) !important;
+    background: -webkit-linear-gradient(left, #3FC2F1  0%, #3479DE 100%) !important;
+    background: linear-gradient(to right, #3FC2F1 0%,#3479DE 100%) !important;
+  }
+  .merchant-slide-enter {
+    opacity: 0;
+  }
+  .merchant-slide-enter-active {
+    transition: all 0.1s ease;
+  }
+  .merchant-slide-leave {
+    opacity: 1;
+  }
+  .merchant-slide-leave-active {
+    transition: all 0.1s ease;
+    opacity: 0;
+  }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
-
 Vue.use(Router)
+
+const shopMain = r => require.ensure([], () => r(require('../pages/shopMain.vue')), 'shopMain')
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'shopMain',
+      component: shopMain,
+      meta: {
+        title: '店铺首页',
+        home: true
+      }
     }
   ]
 })
